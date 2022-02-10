3d1ff387c317323f1d2eb7670877ef4b3d2b12f5
Pedro Zier <41027931+pedrozier@users.noreply.github.com>
2022-02-10 08:43:33 -0300

    Add files via upload

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = segundo => 10s ");
+            Console.WriteLine("M = Minuto => 1m ");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar? ");
+
+            string data = Console.ReadLine().ToLower();
+
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+
+            int multiplier = 1;
+
+            if (type == 'm')
+            {
+                multiplier = 60;
+            }
+            else
+            {
+                multiplier = 1;
+            }
+            if (time == 0)
+            {
+                System.Environment.Exit(0);
+            }
+
+            PreStart(time * multiplier);
+        }
+
+        static void Start(int time)
+        {
+
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado");
+            Thread.Sleep(2500);
+
+            Menu();
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(1000);
+
+            Start(time);
+        }
+    }
+}
+
+
