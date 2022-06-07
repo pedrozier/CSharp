4961a2d640cd78c299a4aab45d19e6119892db73
Pedro Zier <41027931+pedrozier@users.noreply.github.com>
2022-06-06 21:15:28 -0300

    Add files via upload

---
+++ APITesteCSharp/APITesteCSharp/Models/Issue.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APITesteCSharp.Models
+{
+    public class Issue
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public String Description { get; set; }
+
+        public Priority priority { get; set; }
+
+        public IssueType type { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime? Completted { get; set; }
+    }
+
+    public enum Priority
+    {
+        Low,Medium,High
+    }
+    public enum IssueType
+    {
+        Feature,Bug,Documentation
+    }
+
+}
