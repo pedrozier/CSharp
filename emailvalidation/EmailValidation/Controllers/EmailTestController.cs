5669782f61c60b06b1c5b904690f1a99223d17cd
Pedro Zier <pedrozier2003@gmail.com>
2022-06-09 11:54:50 -0300

    api simples para verificar emails

---
+++ emailvalidation/EmailValidation/Controllers/EmailTestController.cs
@@ -0,0 +1,19 @@
+﻿using EmailValidation.Utils;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmailValidation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailTestController : ControllerBase
+    {
+
+        [HttpPost("validateEmail")]
+        public bool TestEmail(string email)
+        {
+            return new EmailAddressAttribute().IsValid(email);
+        }
+    }
+}
