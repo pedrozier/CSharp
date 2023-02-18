da170707360c43583d9a15dc9e59637b8bd44788
Pedro Zier <41027931+pedrozier@users.noreply.github.com>
2023-02-18 08:03:09 -0300

    Update Program.cs

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -13,13 +13,16 @@ namespace ConsoleApp
 
         public static void Menu(string returningMessage) 
         {
+
+
+
+
+                                                                                                   
             Console.Clear(); 
-            Console.WriteLine(" __  __         ____                                      ");
-            Console.WriteLine("|  \\/  |_   _  |  _ \\ _ __ ___   __ _ _ __ __ _ _ __ ___  ");
-            Console.WriteLine("| |\\/| | | | | | |_) | '__/ _ \\ / _` | '__/ _` | '_ ` _ \\ ");
-            Console.WriteLine("| |  | | |_| | |  __/| | | (_) | (_| | | | (_| | | | | | |");
-            Console.WriteLine("|_|  |_|\\__, | |_|   |_|  \\___/ \\__, |_|  \\__,_|_| |_| |_|");
-            Console.WriteLine("        |___/                   |___/                     ");
+            Console.WriteLine("  _____         _          ___  _      _        _           ___                 ___       _    ___ ");
+            Console.WriteLine(" |_   _|__ _ _ | |_  ___  |   \\(_)_ _ | |_  ___(_)_ _ ___  | _ \__ _ _ _ __ _  / __| __ _(_)_ |__ \\");
+            Console.WriteLine("   | |/ -_) ' \\| ' \\/ _ \ | |) | | ' \\| ' \\/ -_) | '_/ _ \\ |  _/ _` | '_/ _` | \\__ \\/ _` | | '_|/_/");
+            Console.WriteLine("   |_|\\___|_||_|_||_\\___/ |___/|_|_||_|_||_\___|_|_| \\___/ |_| \__,_|_| \__,_| |___/\__,_|_|_| (_) ");
             Console.WriteLine(" (v 0.0.1)   (com mais firulas do que nunca!)");
 
             Console.WriteLine("\n" + returningMessage + "\n"); 
