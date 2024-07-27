862f2d1c30c3f82c076f2c882d3b11a3a4a49eee
Pedro Zier <41027931+pedrozier@users.noreply.github.com>
2024-07-27 19:14:36 -0300

    adding project

--- ConsoleApp/obj/Debug/net6.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net6.0/ConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb08ee65ae10b111f7f1a495467fea02f222aa8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
